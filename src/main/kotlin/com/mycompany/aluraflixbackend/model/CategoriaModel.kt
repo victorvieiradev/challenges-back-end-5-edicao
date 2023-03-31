37cdd3fa5926e16a5c6ad037518c6b8bf74ebcc1
Victor Vieira <contatovictorvieira@outlook.com>
2023-03-30 21:40:25 -0300

    cria relacionamento entre entidades

--- src/main/kotlin/com/mycompany/aluraflixbackend/model/CategoriaModel.kt
+++ src/main/kotlin/com/mycompany/aluraflixbackend/model/CategoriaModel.kt
@@ -1,12 +1,10 @@
 package com.mycompany.aluraflixbackend.model
 
-import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
+import jakarta.persistence.*
 import jakarta.validation.constraints.NotBlank
 
 @Entity
+@Table(name = "TB_CATEGORIA")
 data class CategoriaModel(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -14,5 +12,7 @@ data class CategoriaModel(
     @field:NotBlank(message = "O Título não pode estar em branco.")
     var titulo: String?,
     @field:NotBlank(message = "A cor não pode estar em branco")
-    var cor: String?
+    var cor: String?,
+    @field:OneToMany
+    var videos: MutableList<VideoModel>?
 )
--- src/main/kotlin/com/mycompany/aluraflixbackend/model/VideoModel.kt
+++ src/main/kotlin/com/mycompany/aluraflixbackend/model/VideoModel.kt
@@ -1,16 +1,10 @@
 package com.mycompany.aluraflixbackend.model
 
-import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
+import jakarta.persistence.*
 import jakarta.validation.constraints.NotBlank
-import jakarta.validation.constraints.NotEmpty
-import org.hibernate.validator.constraints.Length
-import kotlin.math.max
-import kotlin.math.min
 
 @Entity
+@Table(name = "TB_VIDEOS")
 data class VideoModel(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)///
@@ -20,6 +14,7 @@ data class VideoModel(
     @field:NotBlank(message = "A descrição não pode estar vazia.")
     var descricao: String?,
     @field:NotBlank(message = "A URL não pode estar vazia.")
-    var url: String?
-) {
-}
+    var url: String?,
+    @field:ManyToOne
+    var categoria: CategoriaModel?
+)
